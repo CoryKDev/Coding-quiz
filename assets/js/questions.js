69740e30f83c9469bfd58910d06ac5196f00de7f
CoryKDev <corykeelan7@gmail.com>
2020-11-01 20:11:50 -0800

    submitting homework

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+var jsQuestions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "How do we exit a for loop?",
+    choices: ["Return", "Leave", "What is a for loop?", "You have the source code"],
+    answer: "Return"
+  },
+  {
+    title: "DOM is an abreviation for ____",
+    choices: ["Data Object Mode", "Dusty Old Man", "Document Object Model", "Dude On the Moon"],
+    answer: "Document Object Model"
+  },
+  {
+    title: "JavaScript is Hyper Text Mark-up Language",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "JavaScript is the most popular language used today",
+    choices: ["True", "False"],
+    answer: "True"
+  }
+];
+
+
+
